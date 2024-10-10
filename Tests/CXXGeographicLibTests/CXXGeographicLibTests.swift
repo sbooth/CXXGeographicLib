d311bc129379463e4db713e44db58b361f5b5f5b
Stephen F. Booth <me@sbooth.org>
2024-10-10 10:02:32 -0500

    Test improvements

--- Tests/CXXGeographicLibTests/CXXGeographicLibTests.swift
+++ Tests/CXXGeographicLibTests/CXXGeographicLibTests.swift
@@ -3,6 +3,6 @@ import Testing
 
 @Test func testEllipsoid() {
 	let e = GeographicLib.Ellipsoid(GeographicLib.Constants.WGS84_a(), GeographicLib.Constants.WGS84_f())
-	#expect(e.EquatorialRadius() == GeographicLib.Constants.WGS84_a())
-	#expect(e.Flattening() == GeographicLib.Constants.WGS84_f())
+	#expect(e.EquatorialRadius() == GeographicLib.Ellipsoid.WGS84().pointee.EquatorialRadius())
+	#expect(e.Flattening() == GeographicLib.Ellipsoid.WGS84().pointee.Flattening())
 }
